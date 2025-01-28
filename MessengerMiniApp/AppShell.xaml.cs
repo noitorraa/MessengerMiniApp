4839ffc9886e736042c63fdc1cdd918a8112d5e5
Иван Конюк <ivankonuk00@gmail.com>
2025-01-28 22:52:02 +0700

    Добавьте файлы проекта.

---
+++ MessengerMiniApp/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace MessengerMiniApp
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ MessengerMiniApp/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,10 @@
+﻿using Foundation;
+
+namespace MessengerMiniApp
+{
+    [Register("AppDelegate")]
+    public class AppDelegate : MauiUIApplicationDelegate
+    {
+        protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+    }
+}
